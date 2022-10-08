dd7024d8c846f2a1699151aeec6bd83ac1ecaa10
Jay <jay.webp@gmail.com>
2022-10-08 15:48:40 +1100

    Basic round gameplay works

--- script.js
+++ script.js
@@ -9,12 +9,53 @@ function randomInt(min, max) {
 }
 
 function playRound(playerSelection, computerSelection) {
-    // if playerSelection beats computer selection
-    // fill in result accordingly
-    // else if computerSelection beats playerSelection
-    // fill in results accordingly
+    const playerInput = playerSelection.toLowerCase();
+    const computerInput = computerSelection;
 
-    let result = "You " + "[win/lose]" + "!" + "\n" + "[Winning input (first letter capitalised, all others lowercase)]" + " beats " + "[losing input (all lowercase)]" + ".";
+    let result;
+
+    if (playerInput === computerInput) {
+        result = "You draw!";
+    } else if (playerInput === 'rock') {
+        if (computerInput === 'paper') {
+            // player loses
+            result = getResult('lose', computerInput, playerInput);
+        } else if (computerInput === 'scissors') {
+            // player wins
+            result = getResult('win', playerInput, computerInput);
+        }
+    } else if (playerInput === 'paper') {
+        if (computerInput === 'scissors') {
+            // pc wins
+            result = getResult('lose', computerInput, playerInput);
+        } else if (computerInput === 'rock') {
+            // player wins
+            result = getResult('win', playerInput, computerInput);
+        }
+    } else if (playerInput === 'scissors') {
+        if (computerInput === 'rock') {
+            // pc wins
+            result = getResult('lose', computerInput, playerInput);
+        } else if (computerInput === 'paper') {
+            // player wins
+            result = getResult('win', playerInput, computerInput);
+        }
+    }
+
+    console.log(result);
+}
+
+function getResult (playerOutcome, winner, loser) {
+    let result = "You " + playerOutcome + "!" + "\n" + (winner[0].toUpperCase() + winner.substring(1).toLowerCase()) + " beats " + loser + ".";
 
     return result;
 }
+
+    /* 
+    if (playerInput !== 'rock' || playerInput !== 'paper' || playerInput !== 'scissors') {
+        // prompt input again
+        console.log("Try again");
+    } else {
+        playRound();
+    }
+    */
