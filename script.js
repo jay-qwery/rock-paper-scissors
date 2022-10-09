61ca67f9f7a2f5be2f9691b8de7826ae97531671
Jay <jay.webp@gmail.com>
2022-10-09 17:32:40 +1100

    Score tracking has been added with winner, loser and draw outcomes

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     //randomly returns rock, paper, or scissors
     let output = ["rock", "paper", "scissors"];
@@ -20,29 +23,35 @@ function playRound(playerSelection, computerSelection) {
         if (computerInput === 'paper') {
             // player loses
             result = getResult('lose', computerInput, playerInput);
+            computerScore++;
         } else if (computerInput === 'scissors') {
             // player wins
             result = getResult('win', playerInput, computerInput);
+            playerScore++;
         }
     } else if (playerInput === 'paper') {
         if (computerInput === 'scissors') {
             // pc wins
             result = getResult('lose', computerInput, playerInput);
+            computerScore++;
         } else if (computerInput === 'rock') {
             // player wins
             result = getResult('win', playerInput, computerInput);
+            playerScore++;
         }
     } else if (playerInput === 'scissors') {
         if (computerInput === 'rock') {
             // pc wins
             result = getResult('lose', computerInput, playerInput);
+            computerScore++;
         } else if (computerInput === 'paper') {
             // player wins
             result = getResult('win', playerInput, computerInput);
+            playerScore++;
         }
     }
-
     console.log(result);
+    return (result);
 }
 
 function getResult (playerOutcome, winner, loser) {
@@ -51,11 +60,21 @@ function getResult (playerOutcome, winner, loser) {
     return result;
 }
 
-    /* 
-    if (playerInput !== 'rock' || playerInput !== 'paper' || playerInput !== 'scissors') {
-        // prompt input again
-        console.log("Try again");
-    } else {
-        playRound();
+function game() {
+    for (let i = 0; i < 5; i++) {
+        // Start round
+        let playerChoice = prompt("Rock, paper or scissors?");
+        playRound(playerChoice, getComputerChoice());
     }
-    */
+
+    // then, calculate scores and print the winner of the 5 rounds
+    if (playerScore > computerScore) {
+        console.log("Player wins " + playerScore + " to " + computerScore);
+    } else if (playerScore < computerScore) {
+        console.log("Player loses " + playerScore + " to " + computerScore)
+    } else if (playerScore === computerScore) {
+        console.log("It's a draw");
+    }
+}
+
+game();
